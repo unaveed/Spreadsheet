2a4830ceaf61f27bd17b60425ae3ef166dae6611
Umair Naveed <unaveed@gmail.com>
2014-04-08 15:40:00 -0600

    Initial commit

---
+++ messages.h
@@ -0,0 +1,24 @@
+#ifndef MESSAGES_H
+#define MESSAGES_H
+#include <iostream>
+#include <string>
+
+class Messages
+{
+public:
+    Messages(std::string);
+	Messages(std::string, std::string);
+    void send_message();
+    void receive_message();
+    std::string get_command();
+    std::string get_content();
+private:
+    std::string input;
+    std::string delimiter;
+    std::string command;
+    std::string content;
+    std::string cellName;
+    std::string cellContent;
+};
+
+#endif // MESSAGES_H
