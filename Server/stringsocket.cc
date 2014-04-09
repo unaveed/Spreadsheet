080e3f6259670e5fbc53583c71efd16210a1fbf7
Greg Anderson <greg@infobytes.com>
2014-04-09 10:26:54 -0600

    Have a working multithreaded string socket, working on it's own (not a class)

--- Server/stringsocket.cc
+++ Server/stringsocket.cc
@@ -1,9 +1,5 @@
 // ********************************************************************************
-// ********  C++ Multithreaded chess/chat server example                     ******
-// ********                                                                  ******
-// ********  More information: http://www.codebase.eu/                       ******
-// ********                                                                  ******
-// ********  Compiling : g++ server.cpp -o server -pthread                   ******
+// ********  Compiling : g++ stringsocket.cc -pthread                        ******
 // ********************************************************************************
 
 
@@ -21,8 +17,6 @@
 #include <unistd.h>
 
 #include <errno.h>
-
-using namespace std;
 
 //server functions
 int server_start_listen() ;
@@ -44,19 +38,18 @@ volatile fd_set the_state;
 
 pthread_mutex_t mutex_state = PTHREAD_MUTEX_INITIALIZER;
 
-pthread_mutex_t boardmutex = PTHREAD_MUTEX_INITIALIZER; // mutex locker
+pthread_mutex_t boardmutex = PTHREAD_MUTEX_INITIALIZER; // mutex locker for the chessboard vector.
 
 int main() {
-    cout << "Server started." << endl; // do not forget endl, or it won't display.
+    std::cout << "Server started." << std::endl; // do not forget endl, or it won't display.
 
     // start the main and make the server listen on port 12345
     // server_start_listen(12345) will return the server's fd.
 
     int server_fd = server_start_listen() ;
-    if (server_fd == -1)
-    {
-        cout << "An error occured. Closing program." ;
-        return 1 ;
+    if (server_fd == -1) {
+        std::cout << "An error occured. Closing program." ;
+        return 1;
     }
 
     mainloop(server_fd);
@@ -65,47 +58,45 @@ int main() {
 }
 
 int server_start_listen() {
+	struct addrinfo hostinfo, *res;
 
-struct addrinfo hostinfo, *res;
+	int sock_fd;
 
-int sock_fd;
+	int server_fd; // the fd the server listens on
+	int ret;
+	int yes = 1;
 
-int server_fd; // the fd the server listens on
-int ret;
-int yes = 1;
+	// first, load up address structs with getaddrinfo():
 
-// first, load up address structs with getaddrinfo():
+	memset(&hostinfo, 0, sizeof(hostinfo));
 
-memset(&hostinfo, 0, sizeof(hostinfo));
+	hostinfo.ai_family = AF_UNSPEC;  // use IPv4 or IPv6, whichever
+	hostinfo.ai_socktype = SOCK_STREAM;
+	hostinfo.ai_flags = AI_PASSIVE;     // fill in my IP for me
 
-hostinfo.ai_family = AF_UNSPEC;  // use IPv4 or IPv6, whichever
-hostinfo.ai_socktype = SOCK_STREAM;
-hostinfo.ai_flags = AI_PASSIVE;     // fill in my IP for me
-
-getaddrinfo(NULL, PORT, &hostinfo, &res);
+	getaddrinfo(NULL, PORT, &hostinfo, &res);
 
 
-    server_fd = socket(res->ai_family, res->ai_socktype, res->ai_protocol);
-    //if(server_fd < 0) throw some error;
+	server_fd = socket(res->ai_family, res->ai_socktype, res->ai_protocol);
+	//if(server_fd < 0) throw some error;
 
-    //prevent "Error Address already in use"
-    ret = setsockopt(server_fd, SOL_SOCKET, SO_REUSEADDR, &yes, sizeof(int));
-    // if(ret < 0) throw some error;
+	//prevent "Error Address already in use"
+	ret = setsockopt(server_fd, SOL_SOCKET, SO_REUSEADDR, &yes, sizeof(int));
+	// if(ret < 0) throw some error;
 
-    ret = bind(server_fd, res->ai_addr, res->ai_addrlen);
+	ret = bind(server_fd, res->ai_addr, res->ai_addrlen);
 
-    if(ret != 0)
-    {
-        cout << "error :" << strerror(errno) << endl;
-        return -1 ;
-    }
+	if(ret != 0) {
+		std::cout << "error :" << strerror(errno) << std::endl;
+		return -1 ;
+	}
 
-    ret = listen(server_fd, BACKLOG);
-    //if(ret < 0) throw some error;
+	ret = listen(server_fd, BACKLOG);
+	//if(ret < 0) throw some error;
 
 
 
-return server_fd;
+	return server_fd;
 
 }
 
@@ -139,7 +130,7 @@ if (remote_info.ss_family == AF_INET) {
     inet_ntop(AF_INET6, &s->sin6_addr, ipstr, sizeof ipstr);
 }
 
-std::cout << "Connection accepted from "  << ipstr <<  " using port " << port << endl;
+std::cout << "Connection accepted from "  << ipstr <<  " using port " << port << std::endl;
 
     return new_sd;
 
@@ -147,7 +138,7 @@ std::cout << "Connection accepted from "  << ipstr <<  " using port " << port <<
 
 // This function will send data to the clients fd.
 // data contains the message to be send
-int server_send(int fd, string data) {
+int server_send(int fd, std::string data) {
     int ret;
 
     ret = send(fd, data.c_str(), strlen(data.c_str()),0);
@@ -158,7 +149,6 @@ int server_send(int fd, string data) {
 /// This function runs in a thread for every client, and reads incoming data.
 /// It also writes the incoming data to all other clients.
 void *tcp_server_read(void *arg) {
-
     int rfd;
 
     char buf[MAXLEN];
@@ -170,7 +160,7 @@ void *tcp_server_read(void *arg) {
         //read incoming message.
         buflen = read(rfd, buf, sizeof(buf));
         if (buflen <= 0) {
-            cout << "client disconnected. Clearing fd. " << rfd << endl ;
+            std::cout << "client disconnected. Clearing fd. " << rfd << std::endl;
             pthread_mutex_lock(&mutex_state);
             FD_CLR(rfd, &the_state);      // free fd's from  clients
             pthread_mutex_unlock(&mutex_state);
@@ -181,15 +171,12 @@ void *tcp_server_read(void *arg) {
         // send the data to the other connected clients
         pthread_mutex_lock(&mutex_state);
 
-		cout << "MESSAGE RECEIVED" << endl;
-
-		//memset(&buf[0], 0, sizeof(buf));	// Clear the buffer
-
-        for (wfd = 3; wfd < MAXFD; ++wfd) {
-            if (FD_ISSET(wfd, &the_state) && (rfd != wfd)) {
+		// Send message out to all users
+		for (wfd = 3; wfd < MAXFD; ++wfd) {
+			if (FD_ISSET(wfd, &the_state) && (rfd != wfd)) {
 				// add the users FD to the message to give it an unique ID.
-				string userfd;
-				stringstream out;
+				std::string userfd;
+				std::stringstream out;
 				out << wfd;
 				userfd = out.str();
 				userfd = userfd + ": ";
@@ -201,7 +188,6 @@ void *tcp_server_read(void *arg) {
 		memset(&buf[0], 0, buflen);	// Clear the buffer
 
         pthread_mutex_unlock(&mutex_state);
-
     }
     return NULL;
 }
@@ -209,26 +195,22 @@ void *tcp_server_read(void *arg) {
 // This loop will wait for a client to connect. When the client connects, it creates a
 // new thread for the client and starts waiting again for a new client.
 void mainloop(int server_fd) {
-    string welcome_msg =("Welcome to this telnet chess server. Valid commands are:\n'1' to show the board or '9' to reset the board.\nTo make a move use the board coordinates to change posistion of a piece.\nFor example 'a2a4'\n");
-
     pthread_t threads[MAXFD]; //create 10 handles for threads.
 
     FD_ZERO(&the_state); // FD_ZERO clears all the filedescriptors in the file descriptor set fds.
 
-    while(1) // start looping here
-    {
+	// start looping here
+    while(1) {
         int rfd;
         void *arg; 
 
         // if a client is trying to connect, establish the connection and create a fd for the client.
         rfd = server_establish_connection(server_fd);
 
-        if (rfd >= 0)
-        {
-            cout << "Client connected. Using file desciptor " << rfd << endl;
-            if (rfd > MAXFD)
-            {
-                cout << "To many clients trying to connect." << endl;
+        if (rfd >= 0) {
+            std::cout << "Client connected. Using file desciptor " << rfd << std::endl;
+            if (rfd > MAXFD) {
+                std::cout << "To many clients trying to connect." << std::endl;
                 close(rfd);
                 continue;
             }
@@ -240,8 +222,6 @@ void mainloop(int server_fd) {
             pthread_mutex_unlock(&mutex_state); // End the mutex lock
 
             arg = (void *) rfd;
-
-            server_send(rfd, welcome_msg); // send a welcome message/instructions.
 
             // now create a thread for this client to intercept all incoming data from it.
             pthread_create(&threads[rfd], NULL, tcp_server_read, arg);
