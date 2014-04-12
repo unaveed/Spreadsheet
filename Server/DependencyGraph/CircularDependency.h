4b4c23bed46df64452efe16ffcb500b3b1c4e584
Greg Anderson <greg@infobytes.com>
2014-04-11 21:11:50 -0600

    Added circular dependency checking code (Jesus)

---
+++ Server/DependencyGraph/CircularDependency.h
@@ -0,0 +1,18 @@
+#include "DependencyGraph.h"
+#include <string>
+
+class CircularDependency
+{
+public:
+	CircularDependency(DependencyGraph* DG);
+	~CircularDependency(void);
+	std::set<std::string>* GetDirectDependents(std::string name);
+	std::vector<std::string>* GetCellsToRecalculate(std::set<std::string>* names);
+	void Visit(std::string start, std::string name, std::set<std::string>* visited, std::vector<std::string>* changed);
+	std::set<std::string> CellsToRecalculate(std::string name);
+
+
+private:
+	DependencyGraph *dg;
+};
+
