747999f9c4a90bbe364058a3fc82ba069ab54154
Greg Anderson <greg@infobytes.com>
2014-04-18 19:34:46 -0600

    Able to split formulas and extract cells, still converting from c#

--- Server/spreadsheet.cc
+++ Server/spreadsheet.cc
@@ -5,6 +5,8 @@
 #include <fstream>
 #include <iostream>
 #include <sstream>
+#include <vector>
+#include <boost/regex.hpp>
 
 #include "messages.h"
 #include "spreadsheet.h"
@@ -12,12 +14,14 @@
 
 using namespace std;
 
-spreadsheet::spreadsheet(char * _filename) {
+spreadsheet::spreadsheet(const char * _filename, Messages * _message) {
 	clients    = new set<int>;
 	cells      = new map<string, string>;
 	undo_stack = new stack<string>;
 	version    = 1;
 	filename   = _filename;
+	message	   = _message;
+	dg         = new DependencyGraph();
 
 	open();
 }
@@ -26,6 +30,92 @@ spreadsheet::~spreadsheet() {
 	delete clients;
 	delete cells;
 	delete undo_stack;
+}
+
+
+
+/************************* CONVERTING SPREADSHEET FROM C# **********************************/
+
+string spreadsheet::GetCellContents(string name) {
+	return (*cells)[name];
+}
+
+
+
+
+
+void spreadsheet::SetCellContents(string name, string contents) {
+	dg->ReplaceDependents(name, new set<string>);
+	(*cells)[name] = contents;
+}
+
+void spreadsheet::SetCellContentsFormula(string name, string contents) {
+	dg->ReplaceDependents(name, new set<string>);
+	(*cells)[name] = contents;
+}
+
+vector<string> spreadsheet::GetVariables(string formula) {
+    boost::regex expression("^[_a-zA-Z][a-zA-Z0-9_]*$");
+    boost::cmatch what;
+
+    vector<string> v = GetTokens(formula, ':');
+    vector<string> vars;
+
+    for (vector<string>::iterator it = v.begin(); it != v.end(); ++it) {
+        if(regex_match((*it).c_str(), what, expression))
+            vars.push_back(*it);
+        else
+            continue;
+    }   
+
+    return vars;
+}
+
+
+
+// Cite: http://stackoverflow.com/questions/236129/how-to-split-a-string-in-c
+vector<string> spreadsheet::GetTokens(const string &formula, char delim) {
+        vector<string> elems;
+        split(formula, delim, elems);
+        return elems;
+}
+
+vector<string> & spreadsheet::split(const string &s, char delim, vector<string> &elems) {
+    stringstream ss(s);
+    string item;
+    while (getline(ss, item, delim))
+        elems.push_back(item);
+    return elems;
+}
+// End cite
+
+
+/************************* END CONVERTING **********************************/
+
+
+
+
+
+
+void spreadsheet::make_change(std::string) {
+	version++;
+	stringstream ss;
+	ss << version;
+	//message->edit(ss.str(), name, contents, &clients);
+}
+
+void spreadsheet::undo(){
+	// Check for non-empty stack
+	if(!undo_stack->empty()){
+		std::string edit = undo_stack->top();
+		undo_stack->pop();
+
+		stringstream ss;
+		ss << version;
+
+		//Messages *mess = new Messages("ENTER", edit);
+		//mess->send_message();
+	}
 }
 
 void spreadsheet::open() {
@@ -80,24 +170,6 @@ void spreadsheet::save() {
 	}
 	else
 		cout << "Unable to open file." << endl;
-}
-
-void spreadsheet::undo(){
-	// Check for non-empty stack
-	if(!undo_stack->empty()){
-		std::string edit = undo_stack->top();
-		undo_stack->pop();
-
-		stringstream ss;
-		ss << version;
-
-		//Messages *mess = new Messages("ENTER", edit);
-		//mess->send_message();
-	}
-}
-
-void spreadsheet::make_change(std::string) {
-	version++;
 }
 
 void spreadsheet::add_client(int client) {
--- Server/spreadsheet.h
+++ Server/spreadsheet.h
@@ -6,6 +6,10 @@
 #define SPREADSHEET_H
 #include <map>
 #include <stack>
+#include <vector>
+
+class Messages;
+class DependencyGraph;
 
 /*
  * Represents a working spreadsheet.  It has information on current clients working on it,
@@ -17,9 +21,16 @@ class spreadsheet {
 		std::map<std::string, std::string> *cells;	// All of the cells with their contents
 		std::stack<std::string> *undo_stack;		// All changes that have been made
 		int version;		// Version of the spreadsheet
-		char * filename;	// Current name of the file
+		const char * filename;	// Current name of the file
+		Messages * message;	// Object used to communicate with the clients
+		DependencyGraph * dg;	// Dependency graph used to detect circular dependencies
+
+		std::vector<std::string> GetTokens(const std::string &formula, char delim);
+		std::vector<std::string> &split(const std::string &s, char delim, std::vector<std::string> &elems);
+		std::vector<std::string> GetVariables(std::string formula);
+
 	public:
-		spreadsheet(char *);	// Takes the name of the file to open
+		spreadsheet(const char *, Messages *);	// Takes the name of the file to open
 		~spreadsheet();
 		void make_change(std::string);	// Make change to spreadsheet, making all of the necessary checks
 		void add_client(int);			// Add a client to the working spreadsheet
@@ -28,6 +39,9 @@ class spreadsheet {
 		void undo();					// Performs the undo operation
 		void save();		// Saves the spreadsheet to the file with the stored filename in xml format
 		void open();		// Fills the data structures and variables with the spreadsheet information
+		std::string GetCellContents(std::string);	// Returns the cell contents of the given cell
+		void SetCellContents(std::string, std::string);	// Sets the contents of the cell to the contents
+		void SetCellContentsFormula(std::string name, std::string contents);
 };
 
 #endif
